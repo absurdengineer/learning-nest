16bfbe0e64e6f0f1bed631503c6205e7b7390941
webformulator <mddalam1@gmail.com>
2022-07-09 13:04:26 +0530

    Using Services in Controller.

--- src/coffees/coffees.controller.ts
+++ src/coffees/coffees.controller.ts
@@ -8,7 +8,6 @@ import {
   Param,
   Patch,
   Post,
-  Query,
 } from '@nestjs/common';
 import { CoffeesService } from './coffees.service';
 
@@ -17,30 +16,28 @@ export class CoffeesController {
   constructor(private readonly coffeesService: CoffeesService) {}
 
   @Get()
-  findAll(@Query('page') page, @Query('limit') limit): string {
-    return `This Action returns ${limit} coffees on page #${page}.`;
+  findAll() {
+    return this.coffeesService.findAll();
   }
 
   @Get(':id')
-  findOne(@Param('id') id: string): string {
-    return `This Action returns the specific coffee with id #${id}.`;
+  findOne(@Param('id') id: string) {
+    return this.coffeesService.findOne(id);
   }
 
   @Post()
   @HttpCode(HttpStatus.GONE)
-  create(@Body('name') name: string): string {
-    console.log(name);
-    return 'This Action creates a new Coffee Object.';
+  create(@Body() body) {
+    return this.coffeesService.create(body);
   }
 
   @Patch(':id')
   update(@Param('id') id: string, @Body() body) {
-    console.log(body);
-    return `This Action update #${id} coffee`;
+    return this.coffeesService.update(id, body);
   }
 
   @Delete(':id')
   remove(@Param('id') id: string) {
-    return `This Action removes #${id} coffee`;
+    return this.coffeesService.remove(id);
   }
 }
