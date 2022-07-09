a228ba6c6e2654388d6b6021c312d97b8275cb5d
webformulator <mddalam1@gmail.com>
2022-07-09 21:59:13 +0530

    Created and used update coffee DTO.

--- src/coffees/coffees.controller.ts
+++ src/coffees/coffees.controller.ts
@@ -11,6 +11,7 @@ import {
 } from '@nestjs/common';
 import { CoffeesService } from './coffees.service';
 import { CreateCoffeeDto } from './dto/create-coffee.dto';
+import { UpdateCoffeeDto } from './dto/update-coffee.dto';
 
 @Controller('coffees')
 export class CoffeesController {
@@ -33,8 +34,8 @@ export class CoffeesController {
   }
 
   @Patch(':id')
-  update(@Param('id') id: string, @Body() body) {
-    return this.coffeesService.update(id, body);
+  update(@Param('id') id: string, @Body() updateCoffeeDto: UpdateCoffeeDto) {
+    return this.coffeesService.update(id, updateCoffeeDto);
   }
 
   @Delete(':id')
