cb8f6de6e64d389ca68ccbabee3033a905cd7594
webformulator <mddalam1@gmail.com>
2022-07-09 12:12:14 +0530

    Created Services for Interacting with the Coffees resource.

--- src/coffees/coffees.service.ts
+++ src/coffees/coffees.service.ts
@@ -17,4 +17,30 @@ export class CoffeesService {
       flavors: ['dark', 'strawberry'],
     },
   ];
+
+  findAll() {
+    return this.coffees;
+  }
+
+  findOne(id: string) {
+    return this.coffees.find((coffee) => coffee.id === +id);
+  }
+
+  create(createCoffeeDto: any) {
+    this.coffees.push(createCoffeeDto);
+  }
+
+  update(id: string, updateCoffeeDto: any) {
+    const existingCoffee = this.findOne(id);
+    if (existingCoffee) {
+      // updates the existing entity
+    }
+  }
+
+  remove(id: string) {
+    const coffeeIndex = this.coffees.findIndex((item) => item.id === +id);
+    if (coffeeIndex >= 0) {
+      this.coffees.splice(coffeeIndex, 1);
+    }
+  }
 }
