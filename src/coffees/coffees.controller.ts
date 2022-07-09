517c9aefe9731260cd6b0f57d55451f57b25e7e2
webformulator <mddalam1@gmail.com>
2022-07-09 10:41:29 +0530

    Mapped first Get request in Controller

--- src/coffees/coffees.controller.ts
+++ src/coffees/coffees.controller.ts
@@ -1,4 +1,9 @@
-import { Controller } from '@nestjs/common';
+import { Controller, Get } from '@nestjs/common';
 
 @Controller('coffees')
-export class CoffeesController {}
+export class CoffeesController {
+  @Get()
+  findAll(): string {
+    return 'This Action returns all the coffees.';
+  }
+}
