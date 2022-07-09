e2cdd78f24ef25ddbf3c2622b8bba3912d807190
webformulator <mddalam1@gmail.com>
2022-07-09 20:44:07 +0530

    Sending Custom HttpException for Some Failures.

--- src/coffees/coffees.service.ts
+++ src/coffees/coffees.service.ts
@@ -1,4 +1,4 @@
-import { Injectable } from '@nestjs/common';
+import { HttpException, HttpStatus, Injectable } from '@nestjs/common';
 import { Coffee } from './entities/coffee.entity';
 
 @Injectable()
@@ -23,7 +23,10 @@ export class CoffeesService {
   }
 
   findOne(id: string) {
-    return this.coffees.find((coffee) => coffee.id === +id);
+    const coffee = this.coffees.find((coffee) => coffee.id === +id);
+    if (!coffee)
+      throw new HttpException(`Coffee #${id} not found`, HttpStatus.NOT_FOUND);
+    return coffee;
   }
 
   create(createCoffeeDto: any) {
