5274648f55710c0e11642ed8566e02a504f36df3
webformulator <mddalam1@gmail.com>
2022-07-09 11:17:29 +0530

    Mapped first Patch request in Controller

--- src/coffees/coffees.controller.ts
+++ src/coffees/coffees.controller.ts
@@ -5,24 +5,32 @@ import {
   HttpCode,
   HttpStatus,
   Param,
+  Patch,
   Post,
-  Res,
 } from '@nestjs/common';
 
 @Controller('coffees')
 export class CoffeesController {
   @Get()
-  findAll(@Res() response) {
-    response.status(200).send('This Action returns all the coffees.');
+  findAll(): string {
+    return 'This Action returns all the coffees.';
   }
+
   @Get(':id')
   findOne(@Param('id') id: string): string {
     return `This Action returns the specific coffee with id #${id}.`;
   }
+
   @Post()
   @HttpCode(HttpStatus.GONE)
   create(@Body('name') name: string): string {
     console.log(name);
     return 'This Action creates a new Coffee Object.';
+  }
+
+  @Patch(':id')
+  update(@Param('id') id: string, @Body() body) {
+    console.log(body);
+    return `This Action update #${id} coffee`;
   }
 }
