35f59cf246a6ea92fe6abb0a6bb39e99b1b05f1b
webformulator <mddalam1@gmail.com>
2022-07-09 11:57:20 +0530

    Created Property in service to mock database for now.

--- src/coffees/coffees.service.ts
+++ src/coffees/coffees.service.ts
@@ -1,4 +1,20 @@
 import { Injectable } from '@nestjs/common';
+import { Coffee } from './entities/coffee.entity';
 
 @Injectable()
-export class CoffeesService {}
+export class CoffeesService {
+  private coffees: Coffee[] = [
+    {
+      id: 1,
+      name: 'Latte',
+      brand: 'McDonalds',
+      flavors: ['chocolate', 'vanilla'],
+    },
+    {
+      id: 2,
+      name: 'Mocha',
+      brand: 'Brew Bonny',
+      flavors: ['dark', 'strawberry'],
+    },
+  ];
+}
