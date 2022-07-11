50817b8a62dfcec8d788e65453c4c4b13ec94aa5
webformulator <mddalam1@gmail.com>
2022-07-11 17:22:46 +0530

    Updated User Entityfor Types.

--- src/users/user.entity.ts
+++ src/users/user.entity.ts
@@ -2,7 +2,7 @@ import { Column, Model, Table } from 'sequelize-typescript';
 import { DataTypes } from 'sequelize';
 
 @Table({ tableName: 'users' })
-export class User extends Model {
+export class User extends Model<User> {
   @Column({ type: DataTypes.STRING, allowNull: false })
   name: string;
 
