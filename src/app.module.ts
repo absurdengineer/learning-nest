29730af94d416220b521e71a8fad179f60834b6a
webformulator <mddalam1@gmail.com>
2022-07-11 14:38:14 +0530

    Moved Database to Core as It is a core Module.

--- src/app.module.ts
+++ src/app.module.ts
@@ -2,7 +2,7 @@ import { Module } from '@nestjs/common';
 import { AppController } from './app.controller';
 import { AppService } from './app.service';
 import { CoffeesModule } from './coffees/coffees.module';
-import { DatabaseModule } from './database/database.module';
+import { DatabaseModule } from './core/database/database.module';
 
 @Module({
   imports: [CoffeesModule, DatabaseModule],
