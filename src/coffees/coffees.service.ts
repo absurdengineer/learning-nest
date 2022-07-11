cc788c5e63d2766479a0d35ca7aa8aaae50efac9
webformulator <mddalam1@gmail.com>
2022-07-11 12:16:34 +0530

    Removed Fake Response from Services.

--- src/coffees/coffees.service.ts
+++ src/coffees/coffees.service.ts
@@ -1,48 +1,24 @@
-import { Injectable, NotFoundException } from '@nestjs/common';
-import { Coffee } from './entities/coffee.entity';
+import { Injectable } from '@nestjs/common';
 
 @Injectable()
 export class CoffeesService {
-  private coffees: Coffee[] = [
-    {
-      id: 1,
-      name: 'Latte',
-      brand: 'McDonalds',
-      flavors: ['chocolate', 'vanilla'],
-    },
-    {
-      id: 2,
-      name: 'Mocha',
-      brand: 'Brew Bonny',
-      flavors: ['dark', 'strawberry'],
-    },
-  ];
-
-  findAll() {
-    return this.coffees;
+  async findAll() {
+    return [];
   }
 
   findOne(id: string) {
-    const coffee = this.coffees.find((coffee) => coffee.id === +id);
-    if (!coffee) throw new NotFoundException(`Coffee #${id} not found`);
-    return coffee;
+    return '' + id;
   }
 
   create(createCoffeeDto: any) {
-    this.coffees.push(createCoffeeDto);
+    console.log('Action Invoked' + createCoffeeDto);
   }
 
   update(id: string, updateCoffeeDto: any) {
-    const existingCoffee = this.findOne(id);
-    if (existingCoffee) {
-      // updates the existing entity
-    }
+    console.log('Action Invoked' + updateCoffeeDto);
   }
 
   remove(id: string) {
-    const coffeeIndex = this.coffees.findIndex((item) => item.id === +id);
-    if (coffeeIndex >= 0) {
-      this.coffees.splice(coffeeIndex, 1);
-    }
+    console.log('Action Invoked' + id);
   }
 }
