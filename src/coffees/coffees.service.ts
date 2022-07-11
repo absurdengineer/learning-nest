bb6c58a36e31542e289710877fcee6fb8cc23498
webformulator <mddalam1@gmail.com>
2022-07-11 12:25:29 +0530

    Created Instance of Coffees Provider in Coffees Service.

--- src/coffees/coffees.service.ts
+++ src/coffees/coffees.service.ts
@@ -1,23 +1,26 @@
-import { Injectable } from '@nestjs/common';
+import { Inject, Injectable } from '@nestjs/common';
+import { COFFEE_REPOSITORY } from 'src/core/constants';
+import { Coffee } from './coffee.entity';
 
 @Injectable()
 export class CoffeesService {
+  constructor(
+    @Inject(COFFEE_REPOSITORY)
+    private coffeesRepository: typeof Coffee,
+  ) {}
+
   async findAll() {
     return [];
   }
-
   findOne(id: string) {
     return '' + id;
   }
-
   create(createCoffeeDto: any) {
     console.log('Action Invoked' + createCoffeeDto);
   }
-
   update(id: string, updateCoffeeDto: any) {
     console.log('Action Invoked' + updateCoffeeDto);
   }
-
   remove(id: string) {
     console.log('Action Invoked' + id);
   }
