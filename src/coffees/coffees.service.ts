b82164abb1c2bc80107f87d8ce67c87146b5cf91
webformulator <mddalam1@gmail.com>
2022-07-11 14:37:23 +0530

    Added Exceptions for Not Found.

--- src/coffees/coffees.service.ts
+++ src/coffees/coffees.service.ts
@@ -1,4 +1,4 @@
-import { Inject, Injectable } from '@nestjs/common';
+import { Inject, Injectable, NotFoundException } from '@nestjs/common';
 import { COFFEE_REPOSITORY } from 'src/core/constants';
 import { Coffee } from './coffee.entity';
 import { CreateCoffeeDto } from './dto/create-coffee.dto';
@@ -14,19 +14,27 @@ export class CoffeesService {
   async findAll() {
     return await this.coffeesRepository.findAll();
   }
+
   async findOne(id: number) {
-    return await this.coffeesRepository.findOne({ where: { id } });
+    const coffee = await this.coffeesRepository.findOne({ where: { id } });
+    if (!coffee) throw new NotFoundException('Coffee not found.');
+    return coffee;
   }
+
   async create(createCoffeeDto: CreateCoffeeDto) {
     await this.coffeesRepository.create({
       ...createCoffeeDto,
       flavors: createCoffeeDto.flavors.join(', '),
     });
   }
+
   async update(id: number, updateCoffeeDto: UpdateCoffeeDto) {
-    await this.coffeesRepository.update(updateCoffeeDto, { where: { id } });
+    const coffee = await this.findOne(id);
+    await coffee.update(updateCoffeeDto, { where: { id } });
   }
+
   async remove(id: number) {
-    await this.coffeesRepository.destroy({ where: { id } });
+    const coffee = await this.findOne(id);
+    await coffee.destroy();
   }
 }
