10c901021990292d85f7a51f944229117ca7d2dd
webformulator <mddalam1@gmail.com>
2022-07-11 12:20:51 +0530

    Updated Module to Provide Access of Coffee Providers to Controller.

--- src/coffees/coffees.module.ts
+++ src/coffees/coffees.module.ts
@@ -1,9 +1,10 @@
 import { Module } from '@nestjs/common';
 import { CoffeesController } from './coffees.controller';
 import { CoffeesService } from './coffees.service';
+import { coffeesProviders } from './coffees.providers';
 
 @Module({
   controllers: [CoffeesController],
-  providers: [CoffeesService],
+  providers: [CoffeesService, ...coffeesProviders],
 })
 export class CoffeesModule {}
