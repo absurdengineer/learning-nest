e5da72930bb51eda00c196a854f3f0da106d9bdc
webformulator <mddalam1@gmail.com>
2022-07-12 13:37:14 +0530

    Introduced New Auth Moodule.

--- src/app.module.ts
+++ src/app.module.ts
@@ -4,9 +4,10 @@ import { AppService } from './app.service';
 import { CoffeesModule } from './modules/coffees/coffees.module';
 import { DatabaseModule } from './core/database/database.module';
 import { UsersModule } from './modules/users/users.module';
+import { AuthModule } from './modules/auth/auth.module';
 
 @Module({
-  imports: [CoffeesModule, DatabaseModule, UsersModule],
+  imports: [CoffeesModule, DatabaseModule, UsersModule, AuthModule],
   controllers: [AppController],
   providers: [AppService],
 })
