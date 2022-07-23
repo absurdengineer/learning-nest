300759bbada18f95995b84643a8be3754216cd51
Webformulator <mddalam1@gmail.com>
2022-07-23 13:12:59 +0530

    Mapped Authentication Routes.

--- src/modules/auth/auth.controller.ts
+++ src/modules/auth/auth.controller.ts
@@ -1,4 +1,14 @@
-import { Controller } from '@nestjs/common';
+import { Controller, Post } from '@nestjs/common';
 
 @Controller('auth')
-export class AuthController {}
+export class AuthController {
+  @Post('signup')
+  signUp(): string {
+    return 'This route is for SignUp.';
+  }
+
+  @Post('login')
+  login(): string {
+    return 'This route is for Login.';
+  }
+}
