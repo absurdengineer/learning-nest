f47226dcc3d1c10b0c246fba1036c66f18c8e25e
Webformulator <mddalam1@gmail.com>
2022-07-27 22:25:35 +0530

    Added Validations in Auth Service for username/email

--- src/modules/auth/auth.service.ts
+++ src/modules/auth/auth.service.ts
@@ -1,4 +1,4 @@
-import { Inject, Injectable } from '@nestjs/common';
+import { HttpException, HttpStatus, Inject, Injectable } from '@nestjs/common';
 import { USERS_REPOSITORY } from 'src/core/constants';
 import { CreateUserDto } from '../users/dto/create-user.dto';
 import { User } from '../users/user.entity';
@@ -10,6 +10,20 @@ export class AuthService {
   ) {}
 
   async signUp(createUserDto: CreateUserDto): Promise<User> {
-    return await this.usersRespository.create(createUserDto);
+    let count: number = await this.usersRespository.count({
+      where: { username: createUserDto.username },
+    });
+    if (count > 0)
+      throw new HttpException('Username is taken', HttpStatus.BAD_REQUEST);
+    count = await this.usersRespository.count({
+      where: { email: createUserDto.email },
+    });
+    if (count > 0)
+      throw new HttpException(
+        'Provided email is already in use',
+        HttpStatus.BAD_REQUEST,
+      );
+    const user = await this.usersRespository.create(createUserDto);
+    return user;
   }
 }
