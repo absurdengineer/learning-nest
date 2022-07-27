c40f8f057cb0ea8ec90d01b7ff9adc7e56f51da4
Webformulator <mddalam1@gmail.com>
2022-07-27 20:37:43 +0530

    Implemented Sign up Service.

--- src/modules/auth/auth.module.ts
+++ src/modules/auth/auth.module.ts
@@ -1,9 +1,10 @@
 import { Module } from '@nestjs/common';
+import { usersProviders } from '../users/users.providers';
 import { AuthController } from './auth.controller';
 import { AuthService } from './auth.service';
 
 @Module({
   controllers: [AuthController],
-  providers: [AuthService],
+  providers: [AuthService, ...usersProviders],
 })
 export class AuthModule {}
--- src/modules/auth/auth.service.ts
+++ src/modules/auth/auth.service.ts
@@ -1,4 +1,15 @@
-import { Injectable } from '@nestjs/common';
+import { Inject, Injectable } from '@nestjs/common';
+import { USERS_REPOSITORY } from 'src/core/constants';
+import { CreateUserDto } from '../users/dto/create-user.dto';
+import { User } from '../users/user.entity';
 
 @Injectable()
-export class AuthService {}
+export class AuthService {
+  constructor(
+    @Inject(USERS_REPOSITORY) private usersRespository: typeof User,
+  ) {}
+
+  async signUp(createUserDto: CreateUserDto): Promise<User> {
+    return await this.usersRespository.create(createUserDto);
+  }
+}
