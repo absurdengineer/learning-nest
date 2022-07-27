361a261de51ef943bc4a02a3e457707dec2bcf48
Webformulator <mddalam1@gmail.com>
2022-07-27 22:26:02 +0530

    Implemented Sign Up Functionality.

--- src/modules/auth/auth.controller.ts
+++ src/modules/auth/auth.controller.ts
@@ -1,10 +1,18 @@
-import { Controller, Post } from '@nestjs/common';
+import { Body, Controller, Post } from '@nestjs/common';
+import { CreateUserDto } from '../users/dto/create-user.dto';
+import { User } from '../users/user.entity';
+import { AuthService } from './auth.service';
 
 @Controller('auth')
 export class AuthController {
+  constructor(private readonly authService: AuthService) {}
+
   @Post('signup')
-  signUp(): string {
-    return 'This route is for SignUp.';
+  async signUp(
+    @Body() createUserDto: CreateUserDto,
+  ): Promise<{ data: User; message: string }> {
+    const user = await this.authService.signUp(createUserDto);
+    return { data: user, message: 'Sign up Successfully.' };
   }
 
   @Post('login')
